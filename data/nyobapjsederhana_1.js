4f025e32acdf2c47cd9c116e8a573f7d5ed66da9
felisitastella <76050893+felisitastella@users.noreply.github.com>
2020-12-15 22:19:59 +0700

    Add files via upload

---
+++ data/nyobapjsederhana_1.js
@@ -0,0 +1,8 @@
+var json_nyobapjsederhana_1 = {
+"type": "FeatureCollection",
+"name": "nyobapjsederhana_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.439482113459675, -7.050474968138545 ] } }
+]
+}
